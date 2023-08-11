67fa6d6ab646ba60908b1f2c4808ae2b3fa4fb7b
softstream-link <softstream.link@gmail.com>
2023-08-11 15:43:55 -0400

    checkpoing for soupbin 2way payload refactoring

--- protocols/ouch5/src/connect/svc.rs
+++ protocols/ouch5/src/connect/svc.rs
@@ -56,31 +56,31 @@ mod test {
         let svc_clbk = Ouch5SvcLoggerCallback::new_ref(Level::Info, Level::Debug);
         let clt_clbk = Ouch5CltLoggerCallback::new_ref(Level::Info, Level::Debug);
 
-        let event_store = Ouch5EventStore::new_ref();
+        // let event_store = Ouch5EventStore::new_ref();
         // let svc_clbk = Ouch5SvcEvenStoreCallback::new_ref(Arc::clone(&event_store));
         //     // let clt_clbk = Ouch5CltEvenStoreCallback::new_ref(Arc::clone(&event_store));
 
-        // let svc = Ouch5Svc::bind(*ADDR, svc_clbk, svc_prcl, Some("ouch5/venue"))
-        //     .await
-        //     .unwrap();
+        let svc = Ouch5Svc::bind(*ADDR, svc_clbk, svc_prcl, Some("ouch5/venue"))
+            .await
+            .unwrap();
 
-        // info!("STARTED {}", svc);
-        // let clt = Ouch5Clt::connect(
-        //     *ADDR,
-        //     setup::net::default_connect_timeout(),
-        //     setup::net::default_connect_retry_after(),
-        //     clt_clbk,
-        //     clt_prcl,
-        //     Some("ouch5/broker"),
-        // )
-        // .await
-        // .unwrap();
-        // info!("STARTED {}", clt);
+        info!("STARTED {}", svc);
+        let clt = Ouch5Clt::connect(
+            *ADDR,
+            setup::net::default_connect_timeout(),
+            setup::net::default_connect_retry_after(),
+            clt_clbk,
+            clt_prcl,
+            Some("ouch5/broker"),
+        )
+        .await
+        .unwrap();
+        info!("STARTED {}", clt);
 
-        // let clt_is_connected = clt.is_connected(Some(Duration::from_millis(500))).await;
-        // let svc_is_connected = svc.is_connected(Some(Duration::from_secs(500))).await;
-        // assert!(clt_is_connected);
-        // assert!(svc_is_connected);
+        let clt_is_connected = clt.is_connected(Some(Duration::from_millis(500))).await;
+        let svc_is_connected = svc.is_connected(Some(Duration::from_secs(500))).await;
+        assert!(clt_is_connected);
+        assert!(svc_is_connected);
         //   // tokio::time::sleep(Duration::from_secs(10)).await;
     }
 }
